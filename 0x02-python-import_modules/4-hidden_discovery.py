ad540c343086858b2a2773a133856903262c4d93
JustMakena <justmakenan@gmail.com>
2022-12-02 00:25:49 +0000

    k

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-from add_0 import add
-        
-    a = 1
-    b = 2
+import hidden_4
 
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    for x in dir(hidden_4):
+        if not x.startswith('__'):
+            print(x)
