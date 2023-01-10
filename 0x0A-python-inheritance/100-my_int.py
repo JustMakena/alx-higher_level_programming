ff39c05bd8de20069697d07efbed15c29f9043b3
JustMakena <justmakenan@gmail.com>
2023-01-09 16:47:49 -0800

    int

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-"""Defines a class MyInt that inherits from int."""
+""" Contains the class MyInt """
+
 
 class MyInt(int):
-    """Invert int operators == and !=."""
+    """ Sub Class of int that subverts two methods"""
 
-    def __eq__(self, value):
-    """Override == opeartor with != behavior."""
-    return self.real != value
+    def __new__(cls, *args, **kwargs):
+        """creates a new instance of the class"""
+        return super(MyInt, cls).__new__(cls, *args, **kwargs)
+ 
+    def __ne__(self, other):
+        """ Equal (==) is now Not Equal (!=) """
+        return int(self) == other
 
-    def __ne__(self, value):
-        """Override != operator with == behavior."""
-        return self.real == value
+    def __eq__(self, other):
+         """ Not Equal (=!) is now Equal (==) """
+         return int(self) != other
